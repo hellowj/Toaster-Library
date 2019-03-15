1de0bc44b20e25ea80e681720b2325dc76b56794
Jun Wei <jwei@esri.com>
2019-03-15 11:07:36 +0800

    update

--- lib1/src/main/java/com/example/lib1/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.lib1;
-
-public class Test {
-    public static String getString(){
-        return "lib1";
-    }
-}
--- lib4/src/main/java/com/example/lib4/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.lib4;
-
-public class Test {
-    public static String getString(){
-        return "lib4";
-    }
-}
